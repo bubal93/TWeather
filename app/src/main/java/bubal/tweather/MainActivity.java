1645e4b4461ddf6338310dccb7aa4de1caf5c07e
Alexander Bublik <bublik.alexander93@gmail.com>
2016-03-31 15:58:05 +0200

    Implemented: - 2 databases for storing the information, so the app will work without Internet connection; - one database is for weather forecast, other - for storing the user-picked locations; - database with weather is connected with location database via foreign key; - custom Content Provider, working with URI; - Loaders, so the synchronisation will work correctly even when the app state is changed;

--- app/src/main/java/bubal/tweather/MainActivity.java
+++ app/src/main/java/bubal/tweather/MainActivity.java
@@ -1,10 +1,8 @@
 package bubal.tweather;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
@@ -13,16 +11,23 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final int LAYOUT = R.layout.activity_main;
+
     private final String LOG_TAG = MainActivity.class.getSimpleName();
-    private final String LOG_TAG = MainActivity.class.getSimpleName();
+    private final String FORECASTFRAGMENT_TAG = "FFTAG";
+
+    private String mLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mLocation = Utility.getPreferredLocation(this);
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(LAYOUT);
+
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new ForecastFragment())
+                    .add(R.id.container, new ForecastFragment(), FORECASTFRAGMENT_TAG)
                     .commit();
         }
     }
@@ -48,11 +53,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void openPreferredLocationInMap() {
-        SharedPreferences sharedPrefs =
-                PreferenceManager.getDefaultSharedPreferences(this);
-        String location = sharedPrefs.getString(
-                getString(R.string.pref_location_key),
-                getString(R.string.pref_location_default));
+
+        String location = Utility.getPreferredLocation(this);
 
         // Using the URI scheme for showing a location found on a map
         Uri geoLocation = Uri.parse("geo:0,0?").buildUpon()
@@ -66,6 +68,21 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
         } else {
             Log.d(LOG_TAG, "Couldn't call " + location + ", no receiving apps installed!");
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        String location = Utility.getPreferredLocation(this);
+
+        // Update the location in second pane using the fragment manager
+        if (location != null && !location.equals(mLocation)) {
+            ForecastFragment ff = (ForecastFragment) getSupportFragmentManager().findFragmentByTag(FORECASTFRAGMENT_TAG);
+            if (null != ff) {
+                ff.onLocationChanged();
+            }
+            mLocation = location;
         }
     }
 }
